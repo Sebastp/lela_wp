625130be44c7987c687b9f6ab3d81ce2061fc35f
Steve <urgent.programmer@gmail.com>
2016-10-09 13:00:39 -0400

    Post Read -- Wordpress post meta
    Need foreign key for loopback api - wordpress post relation. Used wordpress post custom meta
    
    https://codex.wordpress.org/Function_Reference/add_post_meta

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ Plugin Name: Lela Studios Loopback
 Description: Reads loopback posts from api and creates wordpress posts. Writes new wordpress posts to loopback api
 */
 
+
 if ( ! wp_next_scheduled( 'leela_loopback_get_posts' ) ) {
   wp_schedule_event( time(), 'hourly', 'leela_loopback_get_posts' );
 }
@@ -11,6 +12,16 @@ if ( ! wp_next_scheduled( 'leela_loopback_get_posts' ) ) {
 add_action( 'leela_loopback_get_posts', 'leela_loopback_read' );
 
 function  leela_loopback_read() {
+    //read api
+    //iterate
+    //get post
+    //compare with existing
+    //update or create
+}
 
+function leela_loopback_post_key($wp_id, $loopback_id) {
+    if ( ! add_post_meta( $wp_id, 'loopback_id', $loopback_id, true ) ) {
+       update_post_meta( $wp_id, 'loopback_id', $loopback_id );
+    }
 }
 ?>
