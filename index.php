0f5bcf84fb3573bb8da5c34282abdb1f7ee194ba
Steve <urgent.programmer@gmail.com>
2016-10-09 11:59:28 -0400

    Post Read -- cron boilerplate
    Copied cron boilerplate from wp docs
    
    Boilerplate for reading posts periodically, and creating posts based on new results from api.
    
    Easier to add posts inside of wordpress, work just the same as posts written in wp-admin. Did
    not pursue plans of hooking the_loop and showing api results in the feed or view. Much more complex
    and will expose api call to webscale traffic

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ Plugin Name: Lela Studios Loopback
 Description: Reads loopback posts from api and creates wordpress posts. Writes new wordpress posts to loopback api
 */
 
-if ( ! wp_next_scheduled( 'my_task_hook' ) ) {
-  wp_schedule_event( time(), 'hourly', 'my_task_hook' );
+if ( ! wp_next_scheduled( 'leela_loopback_get_posts' ) ) {
+  wp_schedule_event( time(), 'hourly', 'leela_loopback_get_posts' );
 }
 
-add_action( 'my_task_hook', 'my_task_function' );
+add_action( 'leela_loopback_get_posts', 'leela_loopback_read' );
 
-function my_task_function() {
-  wp_mail( 'your@email.com', 'Automatic email', 'Automatic scheduled email from WordPress.');
+function  leela_loopback_read() {
+
 }
 ?>
