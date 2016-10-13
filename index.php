594d1d371b779c0e263cb9e925f25c39fceef973
Steve <urgent.programmer@gmail.com>
2016-10-13 11:54:03 -0400

    Added doc blocks to functions

--- index.php
+++ index.php
@@ -6,20 +6,32 @@ Description: Reads loopback posts from api and creates wordpress posts. Writes n
 
 define('LOOPBACK_URL','https://whispering-river-30674.herokuapp.com');
 
+//cron to get posts
 if ( ! wp_next_scheduled( 'leela_loopback_get_posts' ) ) {
   wp_schedule_event( time(), 'hourly', 'leela_loopback_get_posts' );
 }
 
 add_action( 'leela_loopback_get_posts', 'leela_loopback_read' );
 
+//query_var force get posts
 add_action ('wp','leela_loopback_force');
 
+/**
+ * Force api read if loopback query var is set to "run"
+ *
+ * Used by hooking into wp action, allows user to force a api pull instead of waiting for cron
+ */
 function leela_loopback_force() {
     if($_GET['loopback']=='run') {
         leela_loopback_read();
     }
 }
 
+/**
+ * Read posts from api, and upsert posts in wordpress
+ *
+ * Posts store api id in "loopback_id" meta value
+ */
 function  leela_loopback_read() {
     global $user;
     //read api
@@ -59,6 +71,13 @@ function  leela_loopback_read() {
     }
 }
 
+/**
+ * Wrapper for curl to request loopback api
+ *
+ * @param string url Loopback api endpoint
+ *
+ * @return string response of loopback request
+ */
 function leela_curl($url) {
     $curl_handle=curl_init();
     curl_setopt($curl_handle,CURLOPT_URL,$url);
@@ -72,15 +91,38 @@ function leela_curl($url) {
     return $buffer;
 }
 
-function leela_lookup_append_id ($meta, $api_id) {
+/**
+ * Creates array to be used in wp_query for looking up existing API posts
+ *
+ * For each post added to wordpress from loopback api, a meta value exists "loopback_id", storing
+ * the post id of the loopback api
+ * This function builds the parameter for WP_Meta_Query so multiple posts can be retrieved at once
+ * Helpful when iterating through api responses and preventing a query in a loop
+ *
+ * @param mixed[] $meta existing meta arguments, initiate with an empty variable if none exists, and send back to this function in an iterator
+ * @param integer $loopback_id post id from api to include in query
+ *
+ * @return mixed[] $meta same input parameter, with meta lookup element appended
+ */
+function leela_lookup_append_id ($meta, $loopback_id) {
     $meta[]=array(
         'key'       => 'loopback_id',
-        'value'     => $api_id,
+        'value'     => $loopback_id,
         'compare'   => '='
     );
      return $meta;
 }
 
+/**
+ * Runs meta query with meta argument from function parameters and returns results
+ *
+ * Meta arguments needs further processing before being run in wp meta query
+ * After formating meta arguments meta query is run and results are returned
+ *
+ * @param mixed[] $meta array of meta query arguments, which is an associative array with key, value and compare properties
+ *
+ * @return mixed[] WP_Meta_Query result of WP_Meta_Query
+ */
 function leela_lookup_meta($meta) {
      $args = array(
         'meta_query' => array(
@@ -91,12 +133,19 @@ function leela_lookup_meta($meta) {
     return new WP_Meta_Query( $meta_query_args );
 }
 
+/**
+ * Adds 'loopback_id' meta value to post
+ *
+ * Need method to associate loopback api posts with wordpress posts
+ * Accomplished by creating 'loopback_id' meta value custom post option
+ *
+ * @param integer $wp_id wordpress post id
+ * @param integer $loopback_id loopback api post id
+ *
+ */
 function leela_loopback_post_key($wp_id, $loopback_id) {
     if ( ! add_post_meta( $wp_id, 'loopback_id', $loopback_id, true ) ) {
        update_post_meta( $wp_id, 'loopback_id', $loopback_id );
     }
 }
-
-
-
 ?>
